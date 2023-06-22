83c4a90c7c96e4bd6b5a3d0aea9273c38ff71744
vchan-in <17123227+vchan-in@users.noreply.github.com>
2023-06-22 07:03:58 +0530

    fix

--- app/src/main/java/com/vc/ohmybank/repository/DummyData.kt
+++ app/src/main/java/com/vc/ohmybank/repository/DummyData.kt
@@ -7,74 +7,8 @@ object DummyData {
     val userList: List<User> = listOf(
         User(
             id = 0,
-            name = "Darius Gheorghe",
-            email = "darghe98@gmail.com",
-            status = "active"
-        ),
-        User(
-            id = 1,
-            name = "Marius Gheorghe",
-            email = "email0@gmail.com",
-            status = "inactive"
-        ),
-        User(
-            id = 2,
-            name = "Flavius Gheorghe",
-            email = "email1@gmail.com",
-            status = "inactive"
-        ),
-        User(
-            id = 3,
-            name = "Marian Anton",
-            email = "email2@gmail.com",
-            status = "active"
-        ),
-        User(
-            id = 4,
-            name = "Diana Ion",
-            email = "email3@gmail.com",
-            status = "active"
-        ),
-        User(
-            id = 5,
-            name = "Elisabeta Atodiresei",
-            email = "email4@gmail.com",
-            status = "active"
-        ),
-        User(
-            id = 7,
-            name = "Bogdan Berescu",
-            email = "email5@gmail.com",
-            status = "active"
-        ),
-        User(
-            id = 8,
-            name = "Nicoleta Onofrei",
-            email = "email6@gmail.com",
-            status = "active"
-        ),
-        User(
-            id = 10,
-            name = "Nicoleta Onofrei",
-            email = "email6@gmail.com",
-            status = "active"
-        ),
-        User(
-            id = 12,
-            name = "Nicoleta Onofrei",
-            email = "email6@gmail.com",
-            status = "active"
-        ),
-        User(
-            id = 13,
-            name = "Nicoleta Onofrei",
-            email = "email6@gmail.com",
-            status = "active"
-        ),
-        User(
-            id = 14,
-            name = "Nicoleta Onofrei",
-            email = "email6@gmail.com",
+            name = "Data Error",
+            email = "error@error.error",
             status = "active"
         )
     )
